1d35d10eeae3d3fb58e3f78ef91a1a25019b59a6
SRYANG <sarang628@naver.com>
2023-07-17 19:46:33 +0800

    update

--- app/src/main/java/com/sryang/bottomdetectlazycolunm/MainActivity.kt
+++ app/src/main/java/com/sryang/bottomdetectlazycolunm/MainActivity.kt
@@ -4,25 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sryang.bottomdetectlazycolunm.ui.theme.BottomDetectLazyColunmTheme
 import com.sryang.library.BottomDetectingLazyColumn
+import java.util.Objects
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,5 +55,12 @@ fun GreetingPreview() {
 fun MyScreen() {
     val items = (1..100).map { "Item $it" }
 
-    BottomDetectingLazyColumn(items = items)
+    BottomDetectingLazyColumn(
+        items = items.size,
+        onBottom = {
+            Log.d("sryang123", "onBottom")
+        }
+    ) { index ->
+        Text(text = items[index])
+    }
 }
--- library/src/main/java/com/sryang/library/BottomDetectingLazyColumn.kt
+++ library/src/main/java/com/sryang/library/BottomDetectingLazyColumn.kt
@@ -1,6 +1,5 @@
 package com.sryang.library
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,16 +8,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import java.util.Objects
 
 @Composable
 fun BottomDetectingLazyColumn(
-    items: List<String>
+    items: Int,
+    onBottom: ((Void?) -> Unit)? = null,
+    composable: @Composable ((Int) -> Unit)
 ) {
     val scrollState = rememberLazyListState()
 
@@ -28,7 +29,7 @@ fun BottomDetectingLazyColumn(
     ) {
         items(items) { item ->
             // 아이템을 표시하는 코드
-            Text(text = item)
+            composable.invoke(item)
         }
 
         // 하단 감지를 위한 공간 생성
@@ -40,7 +41,9 @@ fun BottomDetectingLazyColumn(
                     .background(Color.Transparent)
             )
             LaunchedEffect(Unit) {
-                Log.d("sryang123", "bottom")
+                onBottom?.let {
+                    onBottom.invoke(null)
+                }
             }
         }
     }
